7c0a35f2d168c0b5d2bb87821b4385de0c294902
Thiago Fontes <particular@thiagofontes.net>
2021-11-25 20:48:27 -0300

    adicionado sistema de limpeza de terminal após finalização

--- Python_jogo_adivinhar_numero_secreto.py
+++ Python_jogo_adivinhar_numero_secreto.py
@@ -4,8 +4,7 @@ print('O jogo foi iniciado')
 
 #   definindo numero secreto e tentativas do player
 numero_secreto = round(random.randrange(1,101))
-total_de_tentativas = 5
-tentativa = 0
+total_de_tentativas = 0
 
 #   definindo dificuldade
 print('Escolha a dificuldade do jogo')
@@ -17,11 +16,9 @@ elif dificuldade == 2:
 elif dificuldade == 3:
     total_de_tentativas = 5
 
-#  loop das jogadas
+#  loop das rodadas
 for rodada in range(1, total_de_tentativas + 1):
-    print('você esta na tentativa:{} de {}'.format(rodada, total_de_tentativas))
-    
-
+    print('você esta na tentativa:{} de {}'.format(rodada, total_de_tentativas))   
     chute_input = input('chute um número para tentar acertar: ')
     chute = int(chute_input)
 
@@ -44,4 +41,7 @@ for rodada in range(1, total_de_tentativas + 1):
         elif(menor):
             print('***********************************************')
             print('Você errou, o numero secreto é maior')
-print(f'o jogo foi finalizado, obrigado por jogar. O número secreto era {numero_secreto}')
+print(f'o jogo foi concluído, obrigado por jogar. O número secreto era: {numero_secreto}. Aguarde um momento que iremos limpar sua tela.')
+
+os.system('sleep 10')
+os.system('clear')
